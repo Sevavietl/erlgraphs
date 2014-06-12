0bf7f44ab6b2ba61b930cdf1d3e58bc75064deba
Vsevolod Vietluzhskykh <sevavietl@gmail.com>
2014-06-12 04:40:11 +0300

    Added adding of the new graphs

--- src/graphs.hrl
+++ src/graphs.hrl
@@ -1,18 +1,18 @@
 %%% Records used for Mnesia database
 
 %% Record for the graphs table
--record(graphs, {name,      % id of the graph
-				 id}). % name of the graph
+-record(graphs, {id,      % id of the graph
+				 name}). % name of the graph
 
 %% Record for the nodes table
--record(vertecies, {graph_id,       % id of the node
-				id, % id of the graph wich consists node
+-record(vertecies, {id,       % id of the node
+				graph_id, % id of the graph wich consists node
 				name,	  % name of the node 
 				x,		  % x screen coordinates of the node
 				y}).	  % y screen coordinates of the node
 
 %% Record for edges table
--record(edges, {u_id,       % id of the edge
-				id,	  % id of the source node
+-record(edges, {id,       % id of the edge
+				u_id,	  % id of the source node
 				v_id, 	  % id of the target node
 				i}).      % weight of the edge
